1ff5e0b11475cee64934880cc8027ebf20950a41
Maxime Rainville <info@maximerainville.com>
2017-12-04 22:09:57 +1300

    Get the basic routes for creating post and retrieving post working

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\User;
+use App\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,8 +15,44 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    $user = $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     $user = $request->user();
+//
+//     return response()->json($user, 200);
+// });
 
-    return response()->json($user, 200);
+Route::prefix('posts')->group(function () {
+    Route::middleware('auth:api')->post('/', function (Request $request) {
+        $userData = $request->user();
+
+
+        $user = User::find($userData->email);
+        if (!$user) {
+            $user = new User();
+        }
+
+        $user->role = $userData->role;
+        $user->nickname = $userData->nickname;
+        $user->name = $userData->name;
+        $user->picture = $userData->picture;
+        $user->email = $userData->email;
+        $user->sub = $userData->sub;
+
+        $user->save();
+
+
+        $post = new Post([
+            'title' => 'Test',
+            'content' => '<p>Hello World</p>'
+        ]);
+        $post->author()->associate($user);
+        $post->save();
+
+        return response()->json($post, 200);
+    });
+
+    Route::get('/', function (Request $request) {
+        $posts = Post::with('author')->orderBy('created_at', 'desc')->get();
+        return response()->json($posts, 200);
+    });
 });
