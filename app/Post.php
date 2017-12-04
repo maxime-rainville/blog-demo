8f94c424c52cdf4d7e85649c6627e6792217000a
Maxime Rainville <info@maximerainville.com>
2017-12-04 22:53:33 +1300

    Add a post summary.

--- app/Post.php
+++ app/Post.php
@@ -15,4 +15,24 @@ class Post extends Eloquent
     {
         return $this->belongsTo('App\\User');
     }
+
+    /**
+     * Compute a summary of the post by stripping out all tags and getting the 10 words.
+     * @return [type] [description]
+     */
+    public function getSummaryAttribute() {
+        $summary = strip_tags($this->content);
+
+        $limit = 10;
+
+        if (str_word_count($summary, 0) > $limit) {
+              $words = str_word_count($summary, 2);
+              $pos = array_keys($words);
+              $summary = substr($summary, 0, $pos[$limit]) . '...';
+        }
+
+      return $summary;
+    }
+
+    protected $appends = array('summary');
 }
